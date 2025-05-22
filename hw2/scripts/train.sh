60e0a2b3236ea266667c9c45f362d90c3b98052b
sadevans <a.s.gorokhova@yandex.ru>
2025-05-22 23:27:52 +0300

    updated scripts

--- hw2/scripts/train.sh
+++ hw2/scripts/train.sh
@@ -1,13 +1,13 @@
 CODE_DIR=./
 
-GPUS=4
+GPUS=5
 N_PROC=1
-GPU=4
+GPU=5
 
-for EXP in  1
+for EXP in  5
 
 do
 echo gan$EXP
 export HYDRA_FULL_ERROR=1
-CUDA_VISIBLE_DEVICES=$GPUS PYTHONPATH=$CODE_DIR python -m torch.distributed.run --master_port 1252 --nproc_per_node $N_PROC $CODE_DIR/code/train.py --config-dir $CODE_DIR/configs --config-name gan$EXP > log 2> mistakes
+CUDA_VISIBLE_DEVICES=$GPUS PYTHONPATH=$CODE_DIR python -m torch.distributed.run --master_port 1253 --nproc_per_node $N_PROC $CODE_DIR/code/train.py --config-dir $CODE_DIR/configs --config-name gan$EXP > log 2> mistakes
 done
